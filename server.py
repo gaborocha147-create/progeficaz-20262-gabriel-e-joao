16b645fb07ce1e1921e86783b7a4d2dbcc0a3093
Gabriel Kenji Nakahara Rocha <gaborocha.147@gmail.com>
2026-09-04 10:43:28 -0300

    Testes iniciais

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template_string, request, redirect
+import json
+
+server = Flask(__name__)
+
+config = server.config
+test_client = server.test_client
+
+@server.route('/')
+def pagina_imoveis():
+    return render_template_string('''
+        <p>Servidor rodando...</p>
+        <p>Acesse /imoveis para acessar a API.</p>
+    ''')
+
