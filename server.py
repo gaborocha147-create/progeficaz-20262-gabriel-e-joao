416c4e3f386aeb6dd1604c60536a6fecfb4d282d
Gabriel Kenji Nakahara Rocha <gaborocha.147@gmail.com>
2026-09-11 10:05:35 -0300

    Connectar banco de dados

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template_string, request, redirect, jsonify
 import json
 import mysql.connector
+from mysql.connector import Error
 from dotenv import load_dotenv
 import os
 
@@ -10,15 +11,29 @@ server = Flask(__name__)
 
 config = server.config
 test_client = server.test_client
+config = {
+    'host': os.getenv('DB_HOST', 'localhost'),  # Obtém o host do banco de dados da variável de ambiente
+    'user': os.getenv('DB_USER'),  # Obtém o usuário do banco de dados da variável de ambiente
+    'password': os.getenv('DB_PASSWORD'),  # Obtém a senha do banco de dados da variável de ambiente
+    'database': os.getenv('DB_NAME', 'db_escola'),  # Obtém o nome do banco de dados da variável de ambiente
+    'port': int(os.getenv('DB_PORT', 3306)),  # Obtém a porta do banco de dados da variável de ambiente
+    'ssl_ca': os.getenv('SSL_CA_PATH')  # Caminho para o certificado SSL
+}
 
+
+# Função para conectar ao banco de dados
 def conectar_db():
-    conn = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
-    return conn
+    """Estabelece a conexão com o banco de dados usando as configurações fornecidas."""
+    try:
+        # Tenta estabelecer a conexão com o banco de dados usando mysql-connector-python
+        conn = mysql.connector.connect(**config)
+        if conn.is_connected():
+            return conn
+    except Error as err:
+        # Em caso de erro, imprime a mensagem de erro
+        print(f"Erro: {err}")
+        return None
+
 
 def converter_para_dict(cursor_result):
     """Converte resultado do cursor em dicionário pra poder virar json."""
